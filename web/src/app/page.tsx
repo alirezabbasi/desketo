d6bcd104a1aabec5a9499d520917e5daa5c02f4c
Alireza <alireza@softixia.com>
2025-03-22 14:27:20 +0330

    Revert react-bootstrap imports to single line in page.tsx

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,13 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Alert from "react-bootstrap/Alert";
+import { Container, Row, Col, Card, Form, Button, Alert } from "react-bootstrap";
 import { getPackages, registerUser, verifyOtp, updateProfile, selectPackage } from "../lib/api";
 
 interface Package {
